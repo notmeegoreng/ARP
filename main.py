a4bde43eaca5bff19c20a0d5c1d85d10b0555767
notmeegoreng <notmeegoreng@gmail.com>
2022-12-04 17:16:01 +0800

    add some comments

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def get_combined_die(dice: dict[int, int], hit_chance: sympy.Rational = sympy.Ra
     for sides, n in dice.items():
         # mul the mgf of die type
         total *= sum(x ** i for i in range(1, sides + 1)) ** n
+    # get coeffs starting with low powers first
     coefficients = sympy.Poly(total).all_coeffs()[::-1]
     coefficients[0] = sum(coefficients) * ((1 - hit_chance) / hit_chance)
     return coefficients
@@ -45,23 +46,35 @@ def death_chance(coefficients, k, n):
 
 
 def death_chances(coefficients, n):
-    # index of coefficients is damage dealt
-    # and value is relative probability
-    s = sum(coefficients)
-    f = len(coefficients) - 1
+    """
+    :param coefficients: list[int]: index of coefficients is damage dealt and value is relative probability
+    :param n: int: amount of enemy health
+    :return: chances, min_turns
+    chances: list of chances of dying from turn number (min_turns) to turn number (n)
+    min_turns: minimum turns to kill the enemy
+    """
+    s = sum(coefficients)  # coeff sum
+    f = len(coefficients) - 1  # idr why i named this f, but its max damage per turn
+
+    # mgf with x = e^t
     x = sympy.symbols('x')
     expr = sympy.Rational(1, s) * sum(c * x ** i for i, c in enumerate(coefficients))
+
     # get chance of dealing 'index' + 1 damage or more
     # remove chance of dealing 0 damage
     chances_final_blow = sympy.Rational(1, s) * np.cumsum(coefficients[:0:-1])[::-1]
     print(chances_final_blow)
+
+    # minimum turns before a kill
     min_turns = (n - 1) // f + 1
     poly = sympy.Poly(expr ** (min_turns - 2))
     chances = []
 
     for k in range(min_turns, n + 1):
         poly *= expr
+        # get chance of dealing enough damage to have health that gets destroyed in one hit
         chances_damage = poly.all_coeffs()[-n:f - n]
+        # dot product with final blow chance to find chances of dying next turn
         chances.append(np.pad(chances_damage, (f - len(chances_damage), 0)) @ chances_final_blow)
     return chances, min_turns
 
